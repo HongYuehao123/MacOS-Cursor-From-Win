c981c0772fc18bab6775cd03eb6e422137578c50
HongYuehao123 <danielhong2007@icloud.com>
2026-09-09 16:02:05 -0400

    Enable native macOS GUI / LSUIElement support for direct Xcode builds by embedding Info.plist via linker

--- Package.swift
+++ Package.swift
@@ -15,7 +15,15 @@ let package = Package(
     targets: [
         .executableTarget(
             name: "WinToMacCursor",
-            path: "Sources/WinToMacCursor"
+            path: "Sources/WinToMacCursor",
+            linkerSettings: [
+                .unsafeFlags([
+                    "-Xlinker", "-sectcreate",
+                    "-Xlinker", "__TEXT",
+                    "-Xlinker", "__info_plist",
+                    "-Xlinker", "Sources/WinToMacCursor/Info.plist"
+                ])
+            ]
         )
     ]
 )
